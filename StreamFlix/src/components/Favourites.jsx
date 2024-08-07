e374ac369848fd04204a91212ede745bf37a4aea
Sanjana <sanjanaasingh6@gmail.com>
2024-08-07 12:29:39 +0530

    Search feature update

--- StreamFlix/src/components/Favourites.jsx
+++ StreamFlix/src/components/Favourites.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Favourites({watchlist}) {
+  const[search,setSearch]=useState('')
+  let handlesearch=(e)=>{
+    setSearch(e.target.value)
+  }
   return (
     <>
       <div className='flex justify-center flex-wrap'>
           <div className=' items-center bg-blue-400 flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold mx-4'>Action</div>
       </div>
-      <div className='flex justify-center my-4'>
-          <input type='text' placeholder=' Serach Movies' className='h-[3-rem] w-[20-rem] border border-gray-600 bg-gray-200 outline-none'/>
 
+      <div className='flex justify-center my-4'>
+        <input 
+          onChange={handlesearch} 
+          value={search} 
+          type='text' 
+          placeholder='Search Movies' 
+          className='h-12 w-64 border border-gray-400 bg-gray-300 rounded-lg shadow-md outline-none px-4'
+        />
       </div>
+
       <div className='overflow-hidden rounded-lg border border-gray-200 m=8'>
         <table className='w-full text-gray-500 text-center'>
           <thead className='border-b-2'>
@@ -21,7 +32,9 @@ function Favourites({watchlist}) {
               </tr>
           </thead>
           <tbody>
-              {watchlist.map((movieObj)=>{
+              {watchlist.filter((movieObj)=>{
+                return movieObj.title.toLowerCase().includes(search.toLocaleLowerCase())
+              }).map((movieObj)=>{
                 return<tr key={movieObj.id} className='border-b-2'>
                         <td className='flex items-center px-6 py-4'>
                           <div className='flex items-center'>
